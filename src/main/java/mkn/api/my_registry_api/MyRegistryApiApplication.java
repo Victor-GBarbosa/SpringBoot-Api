69cf96a4b2da094db4c0ad3f41cdb1c1b8aa3fed
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-07-12 16:05:04 -0300

    first commit

---
+++ src/main/java/mkn/api/my_registry_api/MyRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package mkn.api.my_registry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRegistryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRegistryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mkn/api/my_registry_api/MyRegistryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mkn.api.my_registry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRegistryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
