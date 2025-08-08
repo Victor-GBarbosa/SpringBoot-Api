f30023f503b83af6975f40bcd68866bb4bdfa2ce
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-08-08 18:07:11 -0300

    Product Registration Permission
     now is required a role level 2 or higher to register a product

--- src/main/java/mkn/api/my_registry_api/config/DevConfig.java
+++ src/main/java/mkn/api/my_registry_api/config/DevConfig.java
@@ -7,6 +7,8 @@ import mkn.api.my_registry_api.entities.enums.UserRole;
 import mkn.api.my_registry_api.repositories.CategoryRepository;
 import mkn.api.my_registry_api.repositories.ProductRepository;
 import mkn.api.my_registry_api.repositories.UserRepository;
+import mkn.api.my_registry_api.services.TokenService;
+import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +27,8 @@ public class DevConfig implements CommandLineRunner {
     CategoryRepository categoryRepository;
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    TokenService tokenService;
 
 
     @Override
@@ -41,6 +45,9 @@ public class DevConfig implements CommandLineRunner {
 //
 //        Product produc2 = new Product(newser,9.99, "Outra Coisa", "", "Alguma coisa legal", category);
 //        productRepository.save(produc2);
+
+        
+        System.out.println(tokenService.validadeToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoLWFwaSIsInN1YiI6InZpY3RvcjVAZ21haWwuY29tIiwiZXhwIjoxNzU0Njg4NjE0fQ.yvrWQSIGS_7h92kEXaEkv6T-G2hFPHGT9-8AH0fPnhU"));
         System.out.printf("Let's go, fellas");
     }
 }
